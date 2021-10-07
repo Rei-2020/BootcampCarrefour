c64ec665b96979941edb5de7d34db3a4ce19c5a8
Rei-2020 <renildo.b.alves@gmail.com>
2021-10-07 18:20:57 -0300

    aula de colletions (funcao estendida concluido)

---
+++ src/main/kotlin/me/reialves/TestefucaoEstendida.kt
@@ -0,0 +1,16 @@
+package me.reialves
+
+fun main() {
+    val salarios = arrayOf(
+        "2000.0".toBigDecimal(),
+        "1500.0".toBigDecimal(),
+        "4000.0".toBigDecimal()
+    )
+
+
+    println("--------------------------")
+    println(salarios.somatoria())
+
+    println("--------------------------")
+    println(salarios.media())
+}
