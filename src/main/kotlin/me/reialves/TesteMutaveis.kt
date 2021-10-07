2b285955412283ec13bdc3dfabe4f8609ee00cdd
Rei-2020 <renildo.b.alves@gmail.com>
2021-10-07 17:25:10 -0300

    aula de colletions (teste map concluido)

---
+++ src/main/kotlin/me/reialves/TesteMutaveis.kt
@@ -0,0 +1,37 @@
+package me.reialves
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+
+
+    println("--------------- list -------------")
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+
+    println("-------------------------------------")
+    funcionarios.add( pedro)
+    funcionarios.forEach { println(it) }
+
+
+    println("-------------------------------------")
+    funcionarios.remove( joao)
+    funcionarios.forEach { println(it) }
+
+    val funcionarioSet = mutableSetOf(joao)
+    // funcionarioSet.forEach { println(it) }
+
+
+    println("-------------------------------------")
+
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+
+    println("-------------------------------------")
+    funcionarioSet.remove(maria)
+
+    funcionarioSet.forEach { println(it) }
+}
