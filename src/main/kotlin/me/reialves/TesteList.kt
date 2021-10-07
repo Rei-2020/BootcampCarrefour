3cb3d445323d1890fb5493936526f96887d29d9b
Rei-2020 <renildo.b.alves@gmail.com>
2021-10-07 13:03:33 -0300

    aula de colletions (groupBy e sortedBy)

---
+++ src/main/kotlin/me/reialves/TesteList.kt
@@ -0,0 +1,38 @@
+package me.reialves
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf (joao, pedro, maria)
+
+    funcionarios.forEach{ println(it)}
+
+    println("--------------------------------------")
+    println(funcionarios.find { it.nome == "Maria"} )
+
+    println("--------------------------------------")
+
+    funcionarios
+        .sortedBy {  it.salario }
+        .forEach { println(it)}
+
+    println("--------------------------------------")
+
+    funcionarios
+        .groupBy {  it.tipocontratacao }
+        .forEach { println(it)}
+
+}
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipocontratacao: String
+
+) {
+    override fun toString(): String = """ 
+        Nome: $nome
+        Salario: $salario
+    """.trimIndent()
+    }
